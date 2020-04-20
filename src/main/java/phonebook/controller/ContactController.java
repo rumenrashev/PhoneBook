aef82ecf74aaa5d02e4b3a56713a9aaf67ca443f
Rumen Rashev <57558028+rumenrashev@users.noreply.github.com>
2020-04-20 16:23:16 +0300

    Create ContactCotroller

---
+++ src/main/java/phonebook/controller/ContactController.java
@@ -0,0 +1,34 @@
+package phonebook.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import phonebook.entity.Contact;
+
+import java.util.*;
+
+@Controller
+public class ContactController {
+    private List<Contact> contacts;
+
+    ContactController() {
+        this.contacts = new ArrayList<>();
+    }
+
+
+
+    @GetMapping("/")
+    public ModelAndView index(ModelAndView modelAndView) {
+        modelAndView.setViewName("index.html");
+        modelAndView.addObject("contacts", this.contacts);
+        return modelAndView;
+    }
+    @PostMapping("/")
+    public  String storeContact(Contact contact) {
+        System.out.println(contact.getName());
+        boolean repeat = false;
+        this.contacts.add(contact);
+        return "redirect:/";
+    }
+}

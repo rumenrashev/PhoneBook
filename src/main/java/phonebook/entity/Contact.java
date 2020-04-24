a1f0c329b5d7c893d83789c0a84cb95ef4d09186
Rumen Rashev <57558028+rumenrashev@users.noreply.github.com>
2020-04-24 18:29:44 +0300

    add Last name section

--- src/main/java/phonebook/entity/Contact.java
+++ src/main/java/phonebook/entity/Contact.java
@@ -3,10 +3,20 @@ package phonebook.entity;
 public class Contact {
     private String name;
     private String number;
+    private String lastName;
 
-    public Contact(String name, String number) {
+    public Contact(String name, String number, String lastName) {
         this.name = name;
         this.number = number;
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getName() {
